f2f927c20d15f9ed3c23a7d3dac4a37389d10cf0
Melania Geymonat Ramirez <melania.geymonat@gmail.com>
2026-02-12 11:21:21 +0000

    Improve input handling in bisection search game
    Added error handling for unrecognized hint input in bisection search game.

--- phase-3-in-depth/innovation-programming-ai/MITx 6.00.1 Introduction to Computer Science and Programming Using Python/Exercises + solutions/Unit 3 Bisection search.py
+++ phase-3-in-depth/innovation-programming-ai/MITx 6.00.1 Introduction to Computer Science and Programming Using Python/Exercises + solutions/Unit 3 Bisection search.py
@@ -9,6 +9,7 @@
 
 # guess my number
 
+
 low = 1
 high = 99
 guess = (high + low)//2
@@ -23,3 +24,11 @@ while hint != "c":
         low = guess + 1
     elif hint == "h":
         high = guess - 1
+    else: print("Sorry, I did not understand your input.")
+  
+    
+    guess = int((high + low)//2)
+    print("Is your secret number", guess, " ?")
+    hint = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
+if hint == "c":
+    print("Game over. Your secret number was: ", guess, ".")
